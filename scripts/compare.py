42cb31d7505806a810c060aa86d302418ad30459
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2022-12-16 11:37:27 -0300

    better logic using sets and error message in compare

--- scripts/compare.py
+++ scripts/compare.py
@@ -6,10 +6,18 @@ def compare_data_resource_paths():
   data_resources_names = os.listdir('data')
   data_resources_paths = [os.path.join('data', i) for i in data_resources_names]
   data_resources_paths.remove('data/.gitkeep')
+  data_resources_paths = set(data_resources_paths)
   local_package = Package('datapackage.json')
   datapackage_resouces_paths = [i["path"] for i in local_package.resources]
+  datapackage_resouces_paths = set(datapackage_resouces_paths)
   if data_resources_paths != datapackage_resouces_paths:
-    print("Resources presentes no arquivo datapackage.json diferente dos listados da pasta data.")
+    diff_datapackage_data = datapackage_resouces_paths.difference(data_resources_paths)
+    if diff_datapackage_data:
+      print(f'Recurso(s) no datapackage.json sem arquivo de dados correspondente em data/: {diff_datapackage_data}')
+    
+    diff_data_datapackage = data_resources_paths.difference(datapackage_resouces_paths)
+    if diff_data_datapackage:
+      print(f'Recurso(s) com arquivo de dados em data/ sem correspondente no datapackage.json: {diff_data_datapackage}')
     sys.exit(1)
 
 if __name__ == '__main__':
